db97d83f5fdd5c35055316c1e58a176e872a94e9
Igor.Safiuillin <igor.safiullin@innostage-group.ru>
2020-07-24 00:22:18 +0300

    wip #1

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$("body").on('click', '[href*="#"]', function(e){
+    let fixed_offset = 100;
+    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 500);
+    e.preventDefault();
+});
